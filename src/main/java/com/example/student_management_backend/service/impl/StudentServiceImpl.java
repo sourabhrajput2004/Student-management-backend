8916d71b6bfbce147d9dc27e417ef5cb34919cdf
SOURABH SINGH RAJPUT <83954966+sourabhrajput2004@users.noreply.github.com>
2026-03-05 14:43:46 +0530

    Update StudentServiceImpl.java

--- src/main/java/com/example/student_management_backend/service/impl/StudentServiceImpl.java
+++ src/main/java/com/example/student_management_backend/service/impl/StudentServiceImpl.java
@@ -15,31 +15,30 @@ public class StudentServiceImpl implements StudentService {
     @Autowired
     private StudentRepository studentRepository;
 
-    // ➕ Save or update student
+    
     @Override
     public Student saveStudent(Student student) {
         return studentRepository.save(student);
     }
 
-    // 📄 Get all students
+    
     @Override
     public List<Student> getAllStudents() {
         return studentRepository.findAll();
     }
 
-    // ❌ Delete student by id
+    
     @Override
     public void deleteStudent(Long id) {
         studentRepository.deleteById(id);
     }
 
-    // 🔍 Search student by name
+    
     @Override
     public List<Student> searchByName(String name) {
         return studentRepository.findByNameIgnoreCase(name);
     }
 
-    // 🔍 Search student by course
     @Override
     public List<Student> searchByCourse(String course) {
         return studentRepository.findByCourseIgnoreCase(course);
